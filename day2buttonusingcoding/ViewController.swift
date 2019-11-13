35e06c527fb9c6a47cc779e75de32b39abe8acfd
Vidya <vidya@vidyas-imac.local>
2019-11-13 08:25:20 +0530

    Initial Commit

---
+++ day2buttonusingcoding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day2buttonusingcoding
+//
+//  Created by Vidya on 13/11/19.
+//  Copyright © 2019 Vidya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
